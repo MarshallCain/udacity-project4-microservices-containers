880733bc19ba3d079586e52bb2668a807dd52446
Kushal Sabharwal <kushalsabharwal3009@gmail.com>
2023-09-18 12:38:42 +0000

    Task  Completed Kubernetes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,17 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=kushalsabharwal/udamlmicroservice-api
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deployment udamlmicroservice-api --image=$dockerpath:latest
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+#kubectl port-forward udamlmicroservice-api 8000:80
+kubectl port-forward deployment/udamlmicroservice-api 8000:80

7163999caea6c8034fc500f1b63ef2c61ee0edfd
Kushal Sabharwal <kushalsabharwal3009@gmail.com>
2023-09-18 11:20:35 +0000

    Task #4 Completed Docker image uploaded

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,15 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=kushalsabharwal/udamlmicroservice-api
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 
+docker login -u kushalsabharwal
+docker tag udamlmicroservice-api $dockerpath:latest
+
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:latest
